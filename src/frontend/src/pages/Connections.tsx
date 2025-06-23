6cd93ed40855ebf6784b1f1bfdc7814f012c084c
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2025-06-23 16:25:40 -0300

    update README.MD

--- src/frontend/src/pages/Connections.tsx
+++ src/frontend/src/pages/Connections.tsx
@@ -7,7 +7,7 @@ const Connections = () => {
         <h1 className="text-3xl font-bold mb-6">Connections</h1>
         <div className="text-gray-500 text-lg mt-8">
           This feature (viewing the researchers you follow) will be available soon, as part of the next OrSync update.<br /><br />
-          If the project continues after the end of the course, it would be exciting to evolve this into a true academic social network for researchers—something like a LinkedIn for science!
+          If the project continues after the end of the course, it would be exciting to evolve this into a true academic social network for researchers: something like a LinkedIn for science!
         </div>
       </div>
     </Layout>
